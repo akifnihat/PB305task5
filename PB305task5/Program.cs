055f98c85a1b9615b204146029c7f3a629cbfcd9
akifnihat <akifst@code.edu.az>
2025-04-30 17:26:22 +0400

    feat:added pb305task5

---
+++ PB305task5/Program.cs
@@ -0,0 +1,70 @@
+﻿#region task1
+//int[] ededler = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+//        Console.Write("Tək ədədlər: ");
+//        for (int i = 0; i < ededler.Length; i++)
+//        {
+//            if (ededler[i] % 2 != 0)
+//            {
+//                Console.Write(ededler[i] + " ");
+//            }
+//        }
+
+//        Console.WriteLine();
+
+//        Console.Write("Cüt ədədlər: ");
+//        for (int i = 0; i < ededler.Length; i++)
+//        {
+//            if (ededler[i] % 2 == 0)
+//            {
+//                Console.Write(ededler[i] + " ");
+//            }
+//        }
+#endregion
+
+#region task2
+//int[] ededler = { 11, 2, 33, 44, 5, 6 };
+
+//int cutIndexCemi = 0;
+//int tekIndexCemi = 0;
+
+//for (int i = 1; i < ededler.Length; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        cutIndexCemi += ededler[i];
+//    }
+//    else
+//    {
+//        tekIndexCemi += ededler[i];
+//    }
+//}
+//int ferq = (cutIndexCemi - tekIndexCemi);
+
+//Console.WriteLine("Cut index cemi: " + cutIndexCemi);
+//Console.WriteLine("Tek index cemi: " + tekIndexCemi);
+//Console.WriteLine("Ferq: " + ferq);
+#endregion
+
+#region task3
+//int[] ededler = { 1, 1, 1, 22, 22, 3, 66, 6, 66 };
+
+//for (int i = 0; i < ededler.Length; i++)
+//{
+//    int say = 0;
+
+//    for (int j = 0; j < ededler.Length; j++)
+//    {
+//        if (ededler[i] == ededler[j])
+//        {
+//            say++;
+//        }
+//    }
+
+//    if (say == 2)
+//    {
+//        Console.Write(ededler[i] + " ");
+//    }
+//}
+
+#endregion
